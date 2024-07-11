bb508837d4358ce9541d8d041212dbdcacb7f397
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-07-11 13:56:20 +0500

    filter and repeat

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// 1 misol
+
+// let arr = [1,2,3,4,456,4,6,7]
+// let index = arr.indexOf(4)
+// let new_arr = arr.filter((item)=> arr.indexOf(item) !== arr.lastIndexOf(item))
+// console.log(new_arr)
+
+// 2 misol
+
+// let arr = [1,2,3,4,5]
+// let new_arr = []
+// for(let i = 0; i < arr.length; i++){
+//     new_arr.push(Math.pow(arr[i], 2))
+// }
+// console.log(new_arr)
+
+// 3 misol
+
+// let arr = [ 1, -4, 12, 0, -3, 29, -150]
+// let new_arr = arr.filter((item)=> item > 0)
+// let result = new_arr.reduce((a,b) => a + b, 0)
+// console.log(result)
+
+// 4 misol
+
+// let str = "George Raymond Richard Martin";
+// let words = str.split("");
+// let result = words.filter((item) => item == item.toUpperCase());
+// console.log(result.join(""));
+
+// 5 misol
+
+let str = "Abdullox"
+for(let i = 0; i < str.length; i++){
+    if(){
+        console.log(str[i])
+    }
+}
